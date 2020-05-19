a0d1b9da9531e1b0c48f0158182218274e0728b4
dineshdsv3 <dineshdsv3@gmail.com>
2020-05-19 22:40:27 +0530

    Smart contract updated

--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -3,9 +3,17 @@ pragma solidity ^0.5.0;
 import './ERC721.sol';
 
 contract Color is ERC721Full {
+    string[] public colors;
+    mapping(string => bool) _colorExists;
 
 constructor() ERC721Full("Color", "CARD") public {
 }
 
+    function mint(string memory _color) public {
+       require(!_colorExists[_color],"color already added");
+       uint _id = colors.push(_color);
+       _mint(msg.sender, _id);
+       _colorExists[_color] = true;
+   }
 
 }
