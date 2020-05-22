fdd4c74008c0cfea47895e6a3c6efcff697a792a
dineshdsv3 <dineshdsv3@gmail.com>
2020-05-22 15:48:19 +0530

    Loaded Blockchain data

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,15 @@ import Web3 from 'web3';
 import './App.css';
 
 class App extends Component {
+	state = {
+		account: '',
+		contract: null,
+		colors: [],
+	};
+
 	async componentWillMount() {
 		await this.loadWeb3();
+		await this.loadBlockchainData();
 	}
 
 	async loadWeb3() {
@@ -16,10 +23,48 @@ class App extends Component {
 		} else {
 			window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
 		}
-  }
-  
+	}
+
+	async loadBlockchainData() {
+		const web3 = window.web3;
+		// Load account
+		const accounts = await web3.eth.getAccounts();
+		this.setState({ account: accounts[0] });
+
+		const networkId = await web3.eth.net.getId();
+		const networkData = Color.networks[networkId];
+		if (networkData) {
+			const abi = Color.abi;
+			const address = networkData.address;
+			const contract = new web3.eth.Contract(abi, address);
+			this.setState({ contract });
+		} else {
+			window.alert('Smart contract not deployed to detected network.');
+		}
+	}
+
 	render() {
-		return <div><h1>ERC 721 Token DApp</h1></div>;
+		return (
+			<div>
+				<nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+					<a
+						className="navbar-brand col-sm-3 col-md-2 mr-0"
+						href=""
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						ERC 721 Sample Color Token
+					</a>
+					<ul className="navbar-nav px-3">
+						<li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+							<small className="text-white">
+								<span id="account">{this.state.account}</span>
+							</small>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		);
 	}
 }
 
